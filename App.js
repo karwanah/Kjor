adb13127e89840cd088efc3a892d9ae760c8976a
karwanah <karwan2156@gmail.com>
2022-03-15 17:44:26 +0100

    intro design(not done)

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Touchable } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={{width:128, resizeMode:'contain', bottom:250}} source={require('./assets/img/logo.png')} />
+      <Text style={styles.maintext}>Velkommen!{'\n'} Kjør vekk med en av 100+ biler i Oslo </Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#53A2BE',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  maintext: {
+    color:'#fff',
+    bottom: 350, 
+    fontSize:32, 
+    textAlign:"center", 
+    fontWeight:'500',
+  }
 });
---
+++ styles/styles.js
@@ -0,0 +1,17 @@
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#53A2BE',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    maintext: {
+      color:'#fff',
+      bottom: 350, 
+      fontSize:32, 
+      textAlign:"center", 
+      fontWeight:'500',
+    }
+  });
+  
