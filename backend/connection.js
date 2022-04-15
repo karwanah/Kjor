9b0e6d2bef03ac66db1f7d2e3f7f76e5708fadf1
Karwan Ahmadi <89755353+karwanah@users.noreply.github.com>
2022-04-15 20:34:28 +0200

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,7 @@
 export function Mongo(mongodb){ 
   
   const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://karwan:P3Dforlife@cluster0.ihl43.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const uri = "mongodb+srv://usernamepassword@cluster0.ihl43.mongodb.net/Cluster0?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
