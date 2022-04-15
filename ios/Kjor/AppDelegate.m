f3bf3960940873f4ba8d30b52e1caa74b563b084
karwanah <karwan2156@gmail.com>
2022-04-15 20:36:08 +0200

    mapskey

--- ios/Kjor/AppDelegate.m
+++ ios/Kjor/AppDelegate.m
@@ -30,7 +30,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-[GMSServices provideAPIKey:@"AIzaSyD-BbFmi6NJf3CcCmOVoRVrPPyb7viex4Y"];
+[GMSServices provideAPIKey:@"gmapskeys"];
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
